f487acc9fa55f424b117a3da41e871de44ed07ba
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-02-21 22:06:07 +0600

    Post request send done.

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const cookieParser = require("cookie-parser");
-// const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 7000;
 
 // ----------------------------------------------------Middleware-------------------------------------------
-// const corsOptions = {
-//   origin: ["http://localhost:5173", "http://localhost:5174"],
-//   credentials: true,
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 // corsOptions
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(cookieParser);
 
@@ -36,36 +36,37 @@ async function run() {
     // ------------------------------------------DB Collection------------------------------------------
     const db = client.db("Task-Management-Application");
     const usersCollection = db.collection("users");
+    const taskCollection = db.collection("task");
 
     // ----------------------------------------Generate jwt token-----------------------------------------
-    // app.post("/jwt", async (req, res) => {
-    //   const email = req.body;
-    //   const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
-    //     expiresIn: "365d",
-    //   });
-    //   res
-    //     .cookie("token", token, {
-    //       httpOnly: true,
-    //       secure: process.env.NODE_ENV === "production",
-    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    //     })
-    //     .send({ success: true });
-    // });
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // -----------------------------------------Logout(Check JWT)-----------------------------------------
-    // app.get("/logout", async (req, res) => {
-    //   try {
-    //     res
-    //       .clearCookie("token", {
-    //         maxAge: 0,
-    //         secure: process.env.NODE_ENV === "production",
-    //         sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    //       })
-    //       .send({ success: true });
-    //   } catch (err) {
-    //     res.status(500).send(err);
-    //   }
-    // });
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
 
     // -----------------------------------------save user in database---------------------------------------
     app.post("/users/:email", async (req, res) => {
@@ -84,10 +85,12 @@ async function run() {
       res.send(result);
     });
 
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    // ----------------------------------------Add Task---------------------------------------------------
+    app.post("/add-task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
